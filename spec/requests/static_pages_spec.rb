e73dc010f06558b48ac1a0f558a8ddafd3d75065
houn <good.sergio@gmail.com>
2013-10-22 13:58:53 +0800

    first refactoring of static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,34 +1,32 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) {"Inventory System |"}
 	describe "Home page" do
+		before { visit '/static_pages/home'}
 		it "should have the h1 'Inventory System'" do
-			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Inventory System')
 		end
 		it "should have the right title" do
-			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Inventory System | Home")
+			page.should have_selector('title', :text => "#{base_title} Home")
 		end
 	end
 	describe "Help Page" do
+		before { visit '/static_pages/help'}
 		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
 			page.should have_selector('h1',:text => 'Help')
 		end
 		it "should have the right title" do
-			visit '/static_pages/help'
-			page.should have_selector('title', :text => "Inventory System | Help")
+			page.should have_selector('title', :text => "#{base_title} Help")
 		end
 	end
 	describe "About Page" do
+		before { visit '/static_pages/about'}
 		it "should have the h1 'About'" do
-			visit '/static_pages/about'
 			page.should have_selector('h1',:text => 'About')
 		end
 		it "should have the right title" do
-			visit '/static_pages/about'
-			page.should have_selector('title', :text => "Inventory System | About")
+			page.should have_selector('title', :text => "#{base_title} About")
 		end
 	end
 end
